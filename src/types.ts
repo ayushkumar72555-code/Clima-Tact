d0e676bf8706a60d40a387a9e4c3976c1a621c47
ayushk <ayushkumar72555@gmail.com>
2026-09-25 17:00:49 +0530

    Add application types

---
+++ src/types.ts
@@ -0,0 +1,2 @@
+export type Page = 'Overview'|'Weather'|'Climate'|'Visualizer'|'Simulator'|'Data Lab'
+export type Location = {name:string;country:string;admin1?:string;latitude:number;longitude:number;timezone:string}
